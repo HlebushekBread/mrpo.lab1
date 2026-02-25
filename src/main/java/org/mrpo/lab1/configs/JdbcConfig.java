8c524dc868bddde6a5cec56c538a92e2dd9c9144
Hlebushek <hlebushek.bread@gmail.com>
2026-02-25 16:39:55 +0300

    changed DB configuration to external

--- src/main/java/org/mrpo/lab1/configs/JdbcConfig.java
+++ src/main/java/org/mrpo/lab1/configs/JdbcConfig.java
@@ -1,20 +1,30 @@
 package org.mrpo.lab1.configs;
 
+import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:database.properties")
 public class JdbcConfig {
+
+    private final Environment environment;
+
+    public JdbcConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/shoes");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("Lol228666");
+        dataSource.setDriverClassName(environment.getProperty("database.driver"));
+        dataSource.setUrl(environment.getProperty("database.url"));
+        dataSource.setUsername(environment.getProperty("database.username"));
+        dataSource.setPassword(environment.getProperty("database.password"));
 
         return dataSource;
     }
